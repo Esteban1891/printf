8da3220d8325847c9f9bdf552701870be0e07f80
Esteban1891 <esteban.delahoz15@gmail.com>
2020-03-18 18:38:18 -0500

    advance

--- holberton.h
+++ holberton.h
@@ -19,14 +19,15 @@ int print_c(va_list c);
 int print_s(va_list s);
 int print_i(va_list i);
 int print_d(va_list d);
-int print_p(va_list p);
-int print_S(va_list S);
-int print_r(va_list r);
-int print_R(va_list R);
 int print_u(va_list u);
 int print_b(va_list b);
 int print_o(va_list o);
 int print_x(va_list x);
 int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
+
 
 #endif
